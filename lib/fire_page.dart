1cec87999e44a425a84c41070c0b8848767daec5
Fernando Henrique <loja.info.f.h@gmail.com>
2022-11-16 17:07:08 -0300

    firebase

---
+++ lib/fire_page.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+class FirePage extends StatefulWidget {
+  const FirePage({Key? key}) : super(key: key);
+
+  @override
+  State<FirePage> createState() => _FirePageState();
+}
+
+class _FirePageState extends State<FirePage> {
+  int _likes = 0;
+  late final DatabaseReference _likesRef;
+  late StreamSubscription<DatabaseEvent> _likeSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
+  init() async {
+    _likesRef = FirebaseDatabase.instance.ref('like1');
+    try {
+      final likeSnapshot = await _likesRef.get();
+      _likes = likeSnapshot.value as int;
+    } catch (err) {
+      debugPrint(err.toString());
+    }
+
+    _likeSubscription = _likesRef.onValue.listen((DatabaseEvent event) {
+      setState(() {
+        _likes = (event.snapshot.value ?? 0) as int;
+      });
+    });
+  }
+
+  like() async {
+    await _likesRef.set(ServerValue.increment(1));
+  }
+
+  @override
+  void dispose() {
+    _likeSubscription.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(onPressed: like, icon: const Icon(Icons.thumb_up)),
+            Text(
+              _likes.toString(),
+              style: const TextStyle(fontSize: 20),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
